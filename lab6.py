08644ecc4d63ad98a30435320db4b57909040614
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-15 16:09:14 -0700

    finish code

--- lab6.py
+++ lab6.py
@@ -29,3 +29,29 @@ otherwise if num > 1
   loop three times
     print the output (fruit) to the user 
 """
+
+import random 
+
+def main():
+    for i in range (0, 3):
+        spin()
+
+def spin():
+    rand_num = random.randin(1, 20)
+    output = " "
+    if(rand_num > 15):
+      output = "Cherries"
+    elif(rand_num > 10):
+      output = "Orange"
+    elif(rand_num > 5):
+      output = "Plum"
+    elif(rand_num > 2):
+      output = "Bell"
+    elif(rand_num > 1):
+      output = "Melon"
+    else: 
+      output = "Bar"
+
+    print(output, end=" ")
+
+main()
